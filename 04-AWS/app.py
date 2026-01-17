5facd0eba19d7512973fa1316323b4449182054a
Marwan Gdaiem <gdaiem.mrwn@gmail.com>
2026-01-17 15:47:01 +0100

    AWS EC2 Instance- Setting MLFLOW Tracking Server

--- 04-AWS/app.py
+++ 04-AWS/app.py
@@ -15,6 +15,7 @@ import logging
 
 import os
 
+os.environ["MLFLOW_TRACKING_URI"]="http://13.60.97.252:5000/"
 
 
 logging.basicConfig(level=logging.WARN)
@@ -27,4 +28,60 @@ def eval_metrics(actual,pred):
     return rmse,mae,r2
 
 
-# if __name__=="__main__":
+if __name__=="__main__":
+    # Read in the wine-quality csv file as a pandas DataFrame
+    csv_url = (
+        "http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
+    )
+    try:
+        data = pd.read_csv(csv_url, sep=";")
+    except Exception as e:
+        logger.exception(
+            "Unable to download training & test CSV, check your internet connection. Error: %s", e
+        )
+        sys.exit(1)
+
+    # Split the data into training and test sets. (0.75, 0.25) split.
+    train, test = train_test_split(data)
+
+    # The predicted column is "quality" which is a scalar from [3, 9]
+    train_x = train.drop(["quality"], axis=1)
+    test_x = test.drop(["quality"], axis=1)
+    train_y = train[["quality"]]
+    test_y = test[["quality"]]
+
+    # Read in hyperparameters
+    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
+    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+
+    with mlflow.start_run():
+        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+        lr.fit(train_x, train_y)
+
+        predicted_qualities = lr.predict(test_x)
+
+        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+
+        print("ElasticNet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+        print("  RMSE: %s" % rmse)
+        print("  MAE: %s" % mae)
+        print("  R2: %s" % r2)
+
+        mlflow.log_param("alpha", alpha)
+        mlflow.log_param("l1_ratio", l1_ratio)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("r2", r2)
+        mlflow.log_metric("mae", mae)
+
+        #setup for the remote server AWS
+
+        remote_server_uri = "http://13.60.97.252:5000/"
+        mlflow.set_tracking_uri(remote_server_uri)
+
+        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+        if tracking_url_type_store != "file":
+            mlflow.sklearn.log_model(lr, "model", registered_model_name="ElasticnetWineModel")
+        else:
+            mlflow.sklearn.log_model(lr, "model")
+ 
